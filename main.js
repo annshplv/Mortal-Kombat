728415cb43bd8b7f4221dfd114b9182f1b838d9a
Anna <botjangles_ann@mail.ru>
2021-10-19 10:26:44 +0300

    fix mistakes

--- main.js
+++ main.js
@@ -54,25 +54,45 @@ function createPlayer(playerObj) {
 
 function changeHP(player) {
     const $playerLife = document.querySelector('.player'+ player.player +' .life');
-    player.hp -= Math.ceil(Math.random() * 10);
-    $playerLife.style.width = player.hp + '%';
+    player.hp -= getRandom(20);
  
-    if (player.hp < 0) {
-        $arenas.appendChild(playerLose(player.name));
+    if (player.hp <= 0) {
+        player.hp = 0;
     }
+
+    $playerLife.style.width = player.hp + '%';
 }
 
-function playerLose(name) {
+function playerWins(name) {
     const $loseTitle = createElement('div', 'loseTitle');
-    $loseTitle.innerText = name + ' lose';
+    if (name) {
+        $loseTitle.innerText = name + ' wins';
+    } else {
+        $loseTitle.innerText = 'DRAW';
+    }
 
     return $loseTitle;
 }
 
+function getRandom(num) {
+    return Math.ceil(Math.random() * num)
+}
+
 $randomButton.addEventListener('click', function() {
-    console.log('####: Click Random Button');
     changeHP(player1);
     changeHP(player2);
+
+    if (player1.hp === 0 || player2.hp === 0) {
+        $randomButton.disabled = true;
+    }
+
+    if (player1.hp === 0 && player1.hp < player2.hp) {
+        $arenas.appendChild(playerWins(player2.name));
+    } else if (player2.hp === 0 && player2.hp < player1.hp) {
+        $arenas.appendChild(playerWins(player1.name));
+    } else if (player1.hp === 0 && player2.hp === 0) {
+        $arenas.appendChild(playerWins());
+    }
 })
 
 $arenas.appendChild(createPlayer(player1));
